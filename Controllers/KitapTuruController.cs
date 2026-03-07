75b0557c176839a0103d3b874bf810b4632667f3
Gokalp <gokalppabdullah@gmail.com>
2026-03-07 21:14:15 +0300

    Proje dosyası ekle.

---
+++ Controllers/KitapTuruController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebUygulamaProje1.Models;
+using WebUygulamaProje1.Utility;
+
+namespace WebUygulamaProje1.Controllers
+{
+    public class KitapTuruController : Controller
+    {
+
+        private readonly UygulamaDbContext _uygulamaDbContext;
+
+        public KitapTuruController(UygulamaDbContext context)
+        {
+            _uygulamaDbContext = context;
+        }
+
+
+        public IActionResult Index()   // index action çağırıldığı zaman veritabanına gidip kitap türlerini listeleyecek
+        {
+            List<KitapTuru> objKitapTuruList = _uygulamaDbContext.KitapTurleri.ToList();
+             return View();
+        }
+    }
+}
---
+++ Utility/UygulamaDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebUygulamaProje1.Models;
+
+namespace WebUygulamaProje1.Utility
+{
+    public class UygulamaDbContext : DbContext
+    {
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<KitapTuru> KitapTurleri { get; set; }
+
+    }
+}
