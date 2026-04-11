90db8035c3228f021b0db26e7f1ea28ce95b6ec7
Gokalp <gokalppabdullah@gmail.com>
2026-04-11 23:56:50 +0300

    Repository  design pattern create

---
+++ Models/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace WebUygulamaProje1.Models
+{
+    public interface IRepository<T> where T : class
+    {
+        // T -> KitapTuru
+        IEnumerable<T> GetAll(); //getall ->  tümünü getir
+        T Get(Expression<Func<T, bool>> filtre);
+        void Ekle(T entity);
+        void Sil(T entity);
+        void SilAralik(IEnumerable<T> entities);
+
+
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WebUygulamaProje1.Utility;
+
+namespace WebUygulamaProje1.Models
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly UygulamaDbContext _uygulamaDbContext;
+        internal DbSet<T> dbSet; // dbSet = uygulamaDbContext.KitapTurleri gibi düşünebiliriz. Yani hangi entity ile çalışıyorsak onun dbset'ini tutacak.
+
+        public Repository(UygulamaDbContext uygulamaDbContext)
+        {
+            _uygulamaDbContext = uygulamaDbContext;
+            this.dbSet = _uygulamaDbContext.Set<T>();
+        }
+        public void Ekle(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filtre)
+        {
+            IQueryable<T> sorgu = dbSet;
+            sorgu = sorgu.Where(filtre);
+            return sorgu.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> sorgu = dbSet;
+            return sorgu.ToList();
+        }
+
+        public void Sil(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void SilAralik(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
+ 
